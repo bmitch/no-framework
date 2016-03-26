97900ef7d4992d8360c897cff7264d80ac30a46d
Bill Mitchell <wkmitch@gmail.com>
2016-03-26 00:32:14 +0000

    Step 08

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -21,8 +21,10 @@ if ($environment !== 'production') {
 }
 $whoops->register();
 
-$request = new \Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
-$response = new \Http\HttpResponse;
+$injector = include('Dependencies.php');
+
+$request = $injector->make('Http\HttpRequest');
+$response = $injector->make('Http\HttpResponse');
 
 foreach ($response->getHeaders() as $header) {
     header($header, false);
@@ -53,8 +55,8 @@ switch ($routeInfo[0]) {
 	    $method = $routeInfo[1][1];
 	    $vars = $routeInfo[2];
 
-	    $class = new $className($response);
-	    $class->$method($vars);
+		$class = $injector->make($className);
+		$class->$method($vars);
 	    break;
 }
 
